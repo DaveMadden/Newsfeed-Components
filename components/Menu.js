5162c9004852b4e9c7142add4a4bec49456111ae
Dave Madden <davelearnsstuff@gmail.com>
2021-09-29 12:03:10 -0700

    working on menu button

--- components/Menu.js
+++ components/Menu.js
@@ -21,15 +21,41 @@ const isWorking = "should be able to see this string";
   </div>
 
   The 'menuMaker' takes an array of menu items as its only argument.
+  */
+function menuMaker(items) {
+  const menu = document.createElement('div');
+  const list = document.createElement('ul');
 
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
-  Add those items to the <ul>
+  menu.appendChild(list);
+  
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    list.appendChild(li);
+  })
 
-  Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  const menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('.menu--open');
+  })
 
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  return menu;
+}
 
-  Step 5: Don't forget to return your div.menu.
+// const test = menuMaker(menuItems);
+// console.log(test);
 
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
-*/
+
+  // Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
+  // Add those items to the <ul>
+
+
+
+  // Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+
+  // Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+
+  // Step 5: Don't forget to return your div.menu.
+
+  // Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+
